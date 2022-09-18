fab8509ca9098a2e47cbe4117358a9226dde4b89
daunjeong <qkrtjrfo1@google.com>
2022-09-18 14:38:45 +0900

    Day추가 기능 추가

--- src/component/CreateDay.js
+++ src/component/CreateDay.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 const CreateDay = () => {
-  return <div>날짜추가</div>;
+  const days = useFetch("http://localhost:3001/days");
+  const navigate = useNavigate();
+  function addDay() {
+    fetch(`http://localhost:3001/days/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        day: days.length + 1,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        alert("생성이완료되었습니다.");
+        navigate(`/`);
+      }
+    });
+  }
+
+  return (
+    <div>
+      <h3>현재 일수 : {days.length}일</h3>
+      <button onClick={addDay}>날짜추가</button>
+    </div>
+  );
 };
 
 export default CreateDay;
